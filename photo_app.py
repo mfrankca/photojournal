e66d4d93760b04e35dd3f5e1529884c3cf0dfe9d
Marina frank <mfrankca@yahoo.ca>
2025-03-16 23:58:13 -0400

    initial

---
+++ photo_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import boto3
+import os
+import datetime
+from PIL import Image
+import folium
+from streamlit_folium import folium_static, st_folium
+
+st.write("hello")
